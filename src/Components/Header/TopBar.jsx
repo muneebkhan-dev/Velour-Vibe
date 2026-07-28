0702029bb4b57df5684b34b4bce8ea554da159c2
Muneeb Khan <muneebkhan18069@gmail.com>
2026-07-28 08:15:04 -1200

    feat: add topbar

--- src/Components/Header/TopBar.jsx
+++ src/Components/Header/TopBar.jsx
@@ -4,14 +4,16 @@ import { MdClose } from "react-icons/md"
 const TopBar = () => {
   return (
     <section className='relative bg-black'>
-        <div className="max-w-7xl flex">
-      <div className='text-white py-2 text-base font-light'>
-        Sign up and get 20% off to your first order.
-        <span className='underline font-medium px-2'>Sign Up Now</span>
-      </div>
-      <div className="flex items-center">
-          <MdClose className="text-white"/>ereq
-      </div>
+      <div className="flex justify-end">
+          <div className='font-poppins w-1/2 text-white py-2 text-sm'>
+            Sign up and get 20% off to your first order.
+            <span className='underline font-medium text-sm px-2'>
+              Sign Up Now
+            </span>
+          </div>
+        <div className='flex items-center mr-40'>
+          <MdClose className='text-white size-5' />
+        </div>
       </div>
     </section>
   )
