018c1b2e7afb4be2bc6191d7b123ae55d7d4434d
Muneeb Khan <muneebkhan18069@gmail.com>
2026-07-29 04:00:14 -1200

    feat: add navbar section

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
+import { IoSearch } from "react-icons/io5";
+import { FiShoppingCart } from "react-icons/fi";
+import { CgProfile } from "react-icons/cg";
+
 
 const Navbar = () => {
   return (
-    <nav>
-        <ul className='flex gap-4'>
-            <Link to="/">Home</Link>
-            <Link to="/shop">Shop</Link>
-            <Link to="/onsale">OnSale</Link>
-            <Link to="/newarrivals">NewArrivals</Link>
-            <Link to="/brands">Brands</Link>
+    <nav className="relative bg-amber-300">
+      <div className="max-w-7xl px-3 py-7 mx-auto flex justify-center items-center gap-10">
+        <div>
+          <Link to='/'><h1 className="font-dm font-extrabold text-2xl">VELOUR.CO</h1></Link>
+        </div>
+        <ul className='font-dm flex gap-5'>
+          <Link to='/shop'>Shop</Link>
+          <Link to='/onsale'>On Sale</Link>
+          <Link to='/newarrivals'>New Arrivals</Link>
+          <Link to='/brands'>Brands</Link>
         </ul>
+        <div className="bg-[#F0F0F0] text-[#000000]/40 px-3 py-2 rounded-2xl w-2/5 flex items-center">
+          <IoSearch className="size-6" />
+          <p className="font-dm px-2 text-sm">Search for products...</p>
+        </div>
+        <div className="flex gap-4">
+          <FiShoppingCart className="size-6"/>
+          <CgProfile className="size-6"/>
+        </div>
+      </div>
     </nav>
   )
 }
--- src/Components/Header/TopBar.jsx
+++ src/Components/Header/TopBar.jsx
@@ -4,7 +4,7 @@ import { MdClose } from "react-icons/md"
 const TopBar = () => {
   return (
     <section className='relative bg-black'>
-      <div className="flex justify-end">
+      <div className="'max-w-7xl mx-auto flex justify-end">
           <div className='font-poppins w-1/2 text-white py-2 text-sm'>
             Sign up and get 20% off to your first order.
             <span className='underline font-medium text-sm px-2'>
