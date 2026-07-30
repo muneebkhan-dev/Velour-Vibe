a7c3b4720ddb861508086815a268caff73ea4a83
Muneeb Khan <muneebkhan18069@gmail.com>
2026-07-30 04:23:46 -1200

    feat: add hero section

--- src/pages/Home/Components/Hero.jsx
+++ src/pages/Home/Components/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Hero = () => {
   return (
-    <section className="relative bg-[url('/src/assets/images/hero.png')] bg-cover min-h-screen flex items-center">
-      <div className='max-w-7xl px-10 mx-auto flex flex-col gap-10'>
+    <section className="relative flex items-center bg-[#F2F0F1]">
+      <div className="max-w-7xl px-10 mx-auto bg-[url('/src/assets/images/hero.png')] w-full h-[85vh] bg-center bg-no-repeat min-h-screen flex flex-col justify-center gap-10">
         <div className="w-1/2 flex flex-col gap-6">
           <h1 className='font-dm text-6xl font-extrabold tracking-tighter'>
             FIND CLOTHES THAT MATCHES YOUR STYLE
@@ -14,7 +14,7 @@ const Hero = () => {
             style.
           </p>
           <div>
-          <button className="font-dm text-base bg-black px-16 py-4 rounded-3xl text-white">
+          <button className="font-dm text-base bg-black px-16 py-3 rounded-3xl text-white">
             Shop Now
           </button>
           </div>
@@ -34,9 +34,8 @@ const Hero = () => {
             </div>
         </div>
       </div>
-      <div>
-      <img src="/src/assets/images/Vector (1).png" alt="" />
-      </div>
+      <img src="/src/assets/images/Vector (1).png" alt="" className="w-[7%] absolute top-1/7 right-1/18" />
+      <img src="/src/assets/images/Vector (2).png" alt="" className="absolute top-3/7 right-3/7" />
     </section>
   )
 }
