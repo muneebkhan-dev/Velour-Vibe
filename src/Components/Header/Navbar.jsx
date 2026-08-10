07b98d48d16eef6897bf957dafabc2bbb63043af
Muneeb Khan <muneebkhan18069@gmail.com>
2026-08-10 07:36:00 -1200

    style: make topbar responsive on tablet and mobile

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,29 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "react-router-dom"
 import { IoSearch } from "react-icons/io5";
 import { FiShoppingCart } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
+import { IoMenu } from "react-icons/io5";
 
 
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <nav className="relative">
       <div className="nav-content max-w-7xl px-10 py-6 mx-auto flex justify-between items-center ">
-        <div>
-          <Link to='/'><h1 className="font-dm font-extrabold text-3xl tracking-tight">VELOUR.CO</h1></Link>
+        
+        <div className="flex gap-5">
+          <button
+          className='lg:hidden cursor-pointer z-40'
+          onClick={() => setMenuOpen(true)}
+        >
+          <IoMenu className='size-7' />
+        </button>
+          <Link to='/'><h1 className="font-dm font-extrabold text-2xl lg:text-3xl tracking-tight">VELOUR.CO</h1></Link>
         </div>
-        <ul className='font-dm flex gap-6'>
+        <ul className='font-dm gap-6  hidden lg:flex'>
           <Link className="font-dm text-base" to='/shop'>Shop</Link>
           <Link className="font-dm text-base" to='/onsale'>On Sale</Link>
           <Link className="font-dm text-base" to='/newarrivals'>New Arrivals</Link>
           <Link className="font-dm text-base" to='/brands'>Brands</Link>
         </ul>
-        <div className="bg-[#F0F0F0] text-gray-600 px-4 py-2 rounded-3xl w-2/5 flex items-center">
+        <div className="bg-[#F0F0F0] text-gray-600 px-4 py-2 rounded-3xl w-2/5 hidden lg:flex items-center">
           <IoSearch className="size-5" />
           <input type="text" className="font-dm px-2 placeholder:text-sm outline-hidden" placeholder="Search for products..."/>
           
         </div>
         <div className="nav-icons flex gap-4">
+          <IoSearch className="flex lg:hidden size-6"/>
           <FiShoppingCart className="size-6"/>
           <CgProfile className="size-6"/>
         </div>
--- src/Components/Header/TopBar.jsx
+++ src/Components/Header/TopBar.jsx
@@ -4,14 +4,14 @@ import { MdClose } from "react-icons/md"
 const TopBar = () => {
   return (
     <section className='relative bg-black'>
-      <div className="'max-w-7xl mx-auto flex justify-end">
-          <div className='font-poppins w-1/2 text-white py-2 text-sm'>
+      <div className="'max-w-7xl mx-auto flex justify-center md:justify-end lg:justify-end">
+          <div className='font-poppins md:w-1/2 lg:w-1/2 text-white py-2 text-xs md:text-sm lg:text-sm'>
             Sign up and get 20% off to your first order.
-            <span className='underline font-medium text-sm px-2'>
+            <span className='underline font-medium text-xs md:text-sm lg:text-sm px-2'>
               Sign Up Now
             </span>
           </div>
-        <div className='flex items-center mr-40'>
+        <div className='hidden md:flex lg:flex items-center mr-20 md:mr-40 lg:mr-40'>
           <MdClose className='text-white size-5' />
         </div>
       </div>
