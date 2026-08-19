3153bd37614ea906f1fd7e18368799c8756d187e
Muneeb Khan <muneebkhan18069@gmail.com>
2026-08-19 08:32:46 -1200

    wip(categorygrid section): working on categorygrid secion to make responsive

--- src/pages/Home/Components/CategoryGrid.jsx
+++ src/pages/Home/Components/CategoryGrid.jsx
@@ -5,45 +5,49 @@ const CategoryGrid = () => {
   const Categories = [
     {
       id: 1,
+      title: "Casual",
       img: "/src/assets/images/casual.png",
       isLarge: false,
     },
 
     {
       id: 2,
+      title: "Formal",
       img: "/src/assets/images/formal.png",
       isLarge: true,
     },
 
     {
       id: 3,
+      title: "Party",
       img: "/src/assets/images/party.png",
       isLarge: true,
     },
 
     {
       id: 4,
+      title: "Gym",
       img: "/src/assets/images/gym.png",
       isLarge: false,
     },
   ]
 
   return (
-    <section className='relative flex justify-center py-10 px-15 '>
+    <section className='relative flex justify-center py-10 md:px-10 lg:px-15 '>
       <div className='max-w-7xl w-full bg-[#F0F0F0] rounded-4xl py-15'>
-        <div className='font-dm text-5xl font-extrabold flex justify-center pb-15 '>
+        <div className='font-dm text-4xl lg:text-5xl font-extrabold flex justify-center pb-15 '>
           <h1>BROWSE BY DRESS STYLE</h1>
         </div>
-        <div className='grid grid-cols-3 gap-5 px-20'>
+        <div className='grid grid-cols-3 gap-5 px-5 lg:px-20'>
           {Categories.map((category) => (
               <div
-                className={`relative h-70 rounded-4xl bg-cover bg-center bg-no-repeat ${
+                className={`relative h-[40vh] rounded-4xl bg-cover bg-center bg-no-repeat ${
                   category.isLarge ? "col-span-2" : "col-span-1"
                 }`}
                 key={category.id}
                 style={{ backgroundImage: `url(${category.img})` }}
               >
-                <div>{category.title}</div>
+                <div className="font-dm text-xl lg:text-3xl font-bold p-6">{category.title}</div>
               </div>
             ))}
         </div>
